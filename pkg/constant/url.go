786b7cfb6012bfbd7a4258274564f685097ee765
Fueav <fueave@gmail.com>
2023-06-19 16:00:59 +0800

    update InscriptionUtil

--- pkg/constant/url.go
+++ pkg/constant/url.go
@@ -5,5 +5,6 @@ const (
 	INSCRIPTION_LIST           = "https://api.hiro.so/ordinals/v1/inscriptions?limit=%d&offset=%d%s"
 	INSCRIPTION_LIST_ARGS      = "&order=desc&order_by=genesis_block_height&mime_type=image%2Fsvg%2Bxml&from_number=12205851"
 	INSCRIPTION_LIST_INIT_ARGS = "&to_number=12725973"
-	DATA_CLCT        = "doodinals"
+	DATA_CLCT                  = "doodinals"
+	RCSVIO                     = "rcsv.io"
 )
--- pkg/utils/svg.go
+++ pkg/utils/svg.go
@@ -10,22 +10,32 @@ import (
 var log = logger.Logger("svg")
 
 type InscriptionSvg struct {
-	XMLName  string `xml:"svg"`
-	Style    string `xml:"style,attr"`
-	DataClct string `xml:"data-clct,attr"`
-	Version  string `xml:"version,attr"`
+	XMLName  string  `xml:"svg"`
+	Style    string  `xml:"style,attr"`
+	DataClct string  `xml:"data-clct,attr"`
+	Version  string  `xml:"version,attr"`
+	Image    []Image `xml:"g>image"`
 }
 
-func ContainDataClctUtil(body []byte) (bool, error) {
+type Image struct {
+	XMLName xml.Name `xml:"image"`
+	Href    string   `xml:"href,attr"`
+}
+
+func ContainDataClctUtil(body []byte) (bool, []string, error) {
 	var i InscriptionSvg
 	if err := xml.Unmarshal(body, &i); err != nil {
 		log.Error(err)
-		return false, err
+		return false, []string{}, err
 	}
 
-	if i.DataClct == constant.DATA_CLCT {
+	if i.DataClct == constant.DATA_CLCT || i.DataClct == constant.RCSVIO {
 		log.Info("InscriptionInfo contains data-clct attribute with value 'doodinals'")
-		return true, nil
+		images := make([]string, 0)
+		for index := range i.Image {
+			images = append(images, i.Image[index].Href)
+		}
+		return true, images, nil
 	}
-	return false, errors.New("InscriptionInfo not contains data-clct")
+	return false, []string{}, errors.New("InscriptionInfo not contains data-clct")
 }
